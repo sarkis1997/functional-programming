6b9144131aec67255cc0135a772cdfce24448684
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-11-12 15:20:20 +0100

    mapping newArray data ready

--- src/js/d3-viz.js
+++ src/js/d3-viz.js
@@ -1,9 +1,17 @@
-import { fetchData, fetchedData } from "./fetch.js";
+import { fetchData } from "./utils/fetchFunction.js";
+import { herkomst, subHerkomst } from "./utils/queries.js";
 
-import { herkomst, subHerkomst } from "./utils/querys.js";
 const url = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-07/sparql";
 
-fetchData(url, herkomst);
-fetchData(url, subHerkomst);
-
-console.log(fetchedData)
+fetchData(url, herkomst)
+	.then(data => { const newArr = data.map(
+		item => {
+			let geo = item.herkomstSuperLabel.value;
+			let qty = item.choCount.value;
+			//console.log(geo + ' - ' + qty);
+			return {
+				geo,
+				qty
+			}
+		})
+	});
--- src/js/utils/fetchFunction.js
+++ src/js/utils/fetchFunction.js
@@ -2,15 +2,11 @@ const newQuery = function(x, y) {
 	return (x+"?query="+ encodeURIComponent(y) +"&format=json");
 };
 
-export let fetchedData = [];
-
 export let fetchData = function(url, herkomst) {
 	return fetch(newQuery(url, herkomst))
 		.then(response => response.json(response))
 		.then(data => {
 			let dataRaw = data.results.bindings;
-			dataRaw.forEach(function (item) {
-				fetchedData.push(item);
-			})
+			return dataRaw;
 		})
 };
