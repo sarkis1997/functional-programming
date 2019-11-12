850e696cebbf4750bdb4708837404619613140d8
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-11-12 15:36:40 +0100

    implemented D3

--- src/js/d3-viz.js
+++ src/js/d3-viz.js
@@ -1,17 +1,24 @@
-import { fetchData } from "./utils/fetchFunction.js";
-import { herkomst, subHerkomst } from "./utils/queries.js";
+import {fetchData} from "./utils/fetchFunction.js";
+import {herkomst} from "./utils/queries.js";
 
 const url = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-07/sparql";
 
+let createD3 = function () {
+	d3.select('body')
+		.selectAll('svg')
+		.data(herkomstData)
+}
+
+let herkomstData;
 fetchData(url, herkomst)
-	.then(data => { const newArr = data.map(
+	.then(data => {
+		herkomstData = data.map(
 		item => {
 			let geo = item.herkomstSuperLabel.value;
 			let qty = item.choCount.value;
-			//console.log(geo + ' - ' + qty);
-			return {
-				geo,
-				qty
-			}
-		})
-	});
+
+			return qty
+		});
+	console.log(herkomstData)
+	})
+	.then(createD3);
