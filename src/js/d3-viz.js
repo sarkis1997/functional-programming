5a88bd690bcb43deadade6ba6473a07eae15a36c
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-11-12 22:16:18 +0100

    added d3 force + necessary libararies

--- src/js/d3-viz.js
+++ src/js/d3-viz.js
@@ -2,14 +2,41 @@ import {fetchData} from "./utils/fetchFunction.js";
 import {herkomst} from "./utils/queries.js";
 
 const url = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-07/sparql";
+let herkomstData;
 
 let createD3 = function () {
-	d3.select('body')
-		.selectAll('svg')
-		.data(herkomstData)
-}
+	let width = 500, height = 500;
 
-let herkomstData;
+	let svg = d3.select(".chart")
+		.append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.append("g")
+		.attr("transform", "translate(0,0)");
+
+	let simulation = d3.forceSimulation()
+
+	let circles = svg.selectAll(".artist")
+			.data(herkomstData)
+			.enter().append("circle")
+			.attr("class", "artist")
+			.attr("r", 10)
+			.attr("fill", "lightblue")
+
+	simulation.nodes(herkomstData)
+		.on("tick", ticked)
+
+	function ticked() {
+		circles
+			.attr("cx", function(d) {
+				return d.x
+			})
+			.attr("cy", function(d) {
+				return d.y
+			})
+	}
+};
+
 fetchData(url, herkomst)
 	.then(data => {
 		herkomstData = data.map(
@@ -17,8 +44,11 @@ fetchData(url, herkomst)
 			let geo = item.herkomstSuperLabel.value;
 			let qty = item.choCount.value;
 
-			return qty
+			return {
+				geo,
+				qty
+			};
 		});
-	console.log(herkomstData)
+		console.log(herkomstData)
 	})
 	.then(createD3);
